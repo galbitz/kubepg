ae22afc575558828f87d9d6b28c37fc8a20679c8
Gabor Albitz <galbitz@gmail.com>
2021-01-20 23:44:58 -0500

    basic cluster setup

---
+++ Vagrantfile
@@ -0,0 +1,57 @@
+#
+#  in the wiki:  https://github.com/josenk/vagrant-vmware-esxi/wiki
+
+ENV['VAGRANT_NO_PARALLEL'] = 'yes'
+
+esxi_hostname = '192.168.1.139'
+esxi_username = 'root'
+esxi_password = 'P@ssword1!'
+
+Vagrant.configure('2') do |config|
+
+  config.vm.provision "shell", path: "bootstrap.sh"
+
+  MasterCount = 1
+
+  (1..MasterCount).each do |i|
+    config.vm.define "kmaster#{i}" do |node|
+      node.vm.box = 'generic/ubuntu2004'
+      node.vm.box_check_update = false
+      node.vm.hostname = "kmaster#{i}.example.com"
+      node.vm.network 'public_network', ip: "192.168.1.20#{i}"
+      node.vm.synced_folder('.', '/vagrant', type: 'nfs', disabled: true)
+      node.vm.provider :vmware_esxi do |esxi|
+        esxi.esxi_hostname = esxi_hostname  
+        esxi.esxi_username = esxi_username
+        esxi.esxi_password = esxi_password
+
+        esxi.guest_memsize = '4096'
+      end
+      
+      node.vm.provision "shell", path: "bootstrap_kmaster.sh"
+    end
+  end
+
+  NodeCount = 2
+
+  (1..NodeCount).each do |i|
+    config.vm.define "kworker#{i}" do |node|
+      node.vm.box = 'generic/ubuntu2004'
+      node.vm.box_check_update = false
+      node.vm.hostname = "kworker#{i}.example.com"
+      node.vm.network 'public_network', ip: "192.168.1.21#{i}"
+      node.vm.synced_folder('.', '/vagrant', type: 'nfs', disabled: true)
+      node.vm.provider :vmware_esxi do |esxi|
+        esxi.esxi_hostname = esxi_hostname  
+        esxi.esxi_username = esxi_username
+        esxi.esxi_password = esxi_password
+
+        esxi.guest_memsize = '4096'
+      end
+
+      node.vm.provision "shell", path: "bootstrap_kworker.sh"
+    end    
+  end
+
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# from https://github.com/justmeandopensource/kubernetes
+
+## !IMPORTANT ##
+#
+## This script is tested only in the generic/ubuntu2004 Vagrant box
+## If you use a different version of Ubuntu or a different Ubuntu Vagrant box test this again
+#
+
+echo "[TASK 1] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+echo "[TASK 2] Stop and Disable firewall"
+systemctl disable --now ufw >/dev/null 2>&1
+
+# echo "[TASK 3] Enable and Load Kernel modules"
+# cat >>/etc/modules-load.d/containerd.conf<<EOF
+# overlay
+# br_netfilter
+# EOF
+# modprobe overlay
+# modprobe br_netfilter
+
+# echo "[TASK 4] Add Kernel settings"
+# cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+# net.bridge.bridge-nf-call-ip6tables = 1
+# net.bridge.bridge-nf-call-iptables  = 1
+# net.ipv4.ip_forward                 = 1
+# EOF
+# sysctl --system >/dev/null 2>&1
+
+# echo "[TASK 5] Install containerd runtime"
+# apt update -qq >/dev/null 2>&1
+# apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
+# mkdir /etc/containerd
+# containerd config default > /etc/containerd/config.toml
+# systemctl restart containerd
+# systemctl enable containerd >/dev/null 2>&1
+
+echo "[TASK 5] Install docker "
+apt install -qq -y docker.io >/dev/null 2>&1
+
+echo "[TASK 6] Add apt repo for kubernetes"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - >/dev/null 2>&1
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main" >/dev/null 2>&1
+systemctl enable docker
+systemctl start docker
+
+echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt install -qq -y kubeadm=1.19.0-00 kubelet=1.19.0-00 kubectl=1.19.0-00 >/dev/null 2>&1
+
+echo "[TASK 8] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 9] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 10] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+192.168.1.201   kmaster1.example.com     kmaster1
+192.168.1.202   kmaster2.example.com     kmaster2
+192.168.1.211   kworker1.example.com    kworker1
+192.168.1.212   kworker2.example.com    kworker2
+EOF
